9df896ee404c5809bac0a2527fca1434725d7c15
david54601 <davidzugo@gmail.com>
2020-03-04 14:51:32 -0500

    subido el 04/03/2020

--- components/Products/AddProductForm.js
+++ components/Products/AddProductForm.js
@@ -1,29 +1,54 @@
-import React , {useState,useRef}from "react";
-import {StyleSheet, View, ScrollView,Alert, Dimensions }from "react-native";
-import {Icon, Avatar, Image,Input,Button} from "react-native-elements"
+import React,{useState,useEffect} from "react";
+import {StyleSheet,View,ScrollView,Alert,Dimensions} from "react-native"
+import {Icon,Avatar,Image,Input,Button} from "react-native-elements";
 import * as Permissions from "expo-permissions";
 import * as ImagePicker from "expo-image-picker";
 
+const WidthScreen=Dimensions.get("window").width;
+const WidthMiniature =(WidthScreen/5)-15;
 
 export default function AddProductForm(props){
+    const{toastRef,setIsLoading,navigation}=props;
     const[imagesSelected, setImagesSelected]=useState([]);
-    const{ toastRef,setIsLoading,navigation}=props;
+
+return(
+    <ScrollView>
+        <ImageProduct imageProduct={imagesSelected[0]}/>
+        <UploadImage
+        imagesSelected={imagesSelected}
+        setImagesSelected={setImagesSelected}
+        toastRef={toastRef} 
+
+        />
+    </ScrollView>)
+}
+
+function ImageProduct(props){
+
+    const{imageProduct}=props;
 
     return(
-     <ScrollView>
-         <UploadImage
-          imagesSelected={imagesSelected}
-          setImagesSelected={setImagesSelected}  
-          toastRef={toastRef}       
-         />
-     </ScrollView>
+        <View style={styles.viewPhoto}>
+            {imageProduct?(
+                <Image
+                source={{uri:imageProduct}}
+                style={{width:WidthScreen, height:200}}
 
+                />
+            ):(
+                <Image
+                source={require("../../assets/img/no-photo.png")}
+                style={{width:WidthScreen, height:200}}
+                />
+            )}
+        </View>
     )
-
 }
+
 
 function UploadImage(props){
     const{imagesSelected,setImagesSelected,toastRef}=props;
+
 
     const imageSelect=async()=>{
         const resultPermission =await
@@ -49,36 +74,80 @@ function UploadImage(props){
 
          }
     }
-        console.log(imagesSelected);
+    
+    const removeImage =image=>{
+        const arrayImages=imagesSelected;
+        Alert.alert(
+            "Eliminar imagen",
+            "¿Estas seguro de que desear eliminar la imagen ?",
+            [
+                {
+                    text:"Cancelar",
+                    style:"cancel"
+                },
+                {
+                    text:"Eliminar",
+                    onPress:()=> setImagesSelected(arrayImages.filter(imageUrl=>imageUrl!==image))
+                }
+            ],
+            {cancelable:false}
+        )
+
+    }
+
+
+
     return(
-        <View styles={styles.ViewImages}> 
-        <Icon
-        type="material-community"
-        name="camera"
-        color="#7a7a7a"
-        containerStyle={styles.containerIcon}
-        onPress={imageSelect}
+        <View style={styles.viewImage}>
+        {imagesSelected.length<5&&( 
+            <Icon
+            type="material-community"
+            name="camera"
+            color="#7a7a7a"
+            containerStyle={styles.containerIcon}
+            onPress={imageSelect}
+            />
 
-        />
-        <Avatar
-            onPress={()=> console.log("sdfnskdfdslf")}
-            style={styles.miniatureStyle}
-            //source={{url miniatura}}
-        />
+            )}
 
+        {imagesSelected.map(imageProduct=>(
+
+
+            <Avatar
+            key={imageProduct}
+            onPress={()=>removeImage(imageProduct)}
+            style={styles.miniatureStyles}
+            source={{
+                uri:imageProduct }}            
+            />
+        ))}
+        </View>
+    )
+}
+
+
+function FormAdd(props){
+
+    return(
+        <View style={styles.viewForm}>
 
         </View>
     )
 }
 
+
 const styles=StyleSheet.create({
 
-    ViewImages:{
+    viewForm:{
+        marginLeft:10,
+        marginRight:10
+
+    },
+    viewImage:{
         flexDirection:"row",
         marginLeft:20,
         marginRight:20,
-        marginTop:20
-
+        marginTop:30
     },
     containerIcon:{
         alignItems:"center",
@@ -87,12 +156,20 @@ const styles=StyleSheet.create({
         height:70,
         width:70,
         backgroundColor:"#e3e3e3"
+
     },
-    miniatureStyle:{
-        width:70,
-        height:70,
-        marginRight:10
+    miniatureStyles:{
+        width:WidthMiniature,
+        height:WidthMiniature,
+        marginRight:10,
+
+    },
+    viewPhoto:{
+        alignItems:"center",
+        height:200,
+        marginBottom:20
 
     }
+
 
 })
